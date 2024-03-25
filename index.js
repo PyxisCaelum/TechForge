723489ff535b8636a756e8e2417983ca3e6ff9df
PyxisCaelum <felipealvesbatista28@gmail.com>
2024-03-25 19:53:53 -0300

    Dia 24/03

---
+++ index.js
@@ -0,0 +1,9 @@
+const prompt = require ("prompt-sync")()
+
+let a = prompt ("Primero número: ")
+let b = prompt ("Segundo número: ")
+let c = prompt ("Terceiro número: ")
+let d = prompt ("Quarto número: ")
+
+const soma = a + b + c + d
+console.log = ("Resultado:", soma)

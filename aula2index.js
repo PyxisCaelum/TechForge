9bc0620b67b8363483191ea82f47a74cc93c1461
PyxisCaelum <felipealvesbatista28@gmail.com>
2024-04-18 19:49:23 -0300

    Atualização de um erro com a ancoragem

---
+++ aula2index.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync");
+let number1 = ("Digite seu peso em kg: ");
+let number2 = ("Digite sua altura em cm: ");
+const Math = (number1 / number2 * number2)
+console.log("Seu IMC é de ", Math);
